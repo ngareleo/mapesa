a0381f3db9809e7faa9e09db80a57f0d19ef178a
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-10-15 13:14:59 +0300

    Setup rich component work

--- lib/src/pages/m2/transaction_info_page.dart
+++ lib/src/pages/m2/transaction_info_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mapesa/src/models/transactions/transaction.dart';
+import 'package:mapesa/src/pages/m2/components/transactions_rich_text_component.dart';
 
 class TransactionInfoPageV2 extends StatefulWidget {
-  final Transaction _transaction;
+  final Transaction transaction;
 
-  const TransactionInfoPageV2(this._transaction, {super.key});
+  const TransactionInfoPageV2(this.transaction, {super.key});
 
   @override
   State<TransactionInfoPageV2> createState() => _TransactionInfoPageV2State();
@@ -19,11 +20,23 @@ class _TransactionInfoPageV2State extends State<TransactionInfoPageV2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [Card()],
+      appBar: AppBar(
+        title: const Text("Info"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Card(
+                    child:
+                        mapTransactionToRichTextComponent(widget.transaction))
+              ],
+            ),
+          ),
         ),
       ),
     );
